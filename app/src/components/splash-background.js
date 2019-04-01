518ba799bfd450ec86da290b9479a82a5effb381
Savvas Nicholas <iamyojimbo@gmail.com>
2019-04-01 11:51:28 +0100

    Don't show particles on desktop

--- app/src/components/splash-background.js
+++ app/src/components/splash-background.js
@@ -1,6 +1,8 @@
+// @flow
 import React, { Component } from 'react'
-import styled from 'styled-components'
+import styled, { withTheme } from 'styled-components'
 import Particles from 'react-particles-js'
+import Verge from 'verge'
 
 const Container = styled.div`
   background: #43434d;
@@ -9,17 +11,12 @@ const Container = styled.div`
   position: absolute;
   top: 0;
   left: 0;
-  // background: linear-gradient(
-  //   black 0%,
-  //   transparent 30%,
-  //   ${props => props.theme.color.primary.dark} 100%
-  // );
   background: linear-gradient(
     #0d0d0d,
     ${props => props.theme.color.primary.dark} 30%,
     ${props => props.theme.color.primary.lightest}
   );
-  
+
   .particles {
     ${props => props.theme.media.phone`
       display: none;
@@ -27,64 +24,92 @@ const Container = styled.div`
   }
 `
 
-class SplashBackground extends Component {
+type Props = {
+  theme: any,
+}
+
+type State = {
+  width: number,
+}
+
+class SplashBackground extends Component<Props, State> {
+  state = { width: 0 }
+
+  constructor(props) {
+    super(props)
+  }
+
+  componentDidMount() {
+    this.updateWindowDimensions()
+    window.addEventListener('resize', this.updateWindowDimensions)
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions)
+  }
+
+  updateWindowDimensions = () => this.setState({ width: Verge.viewportW() })
+
   render() {
+    const showParticles = this.state.width > this.props.theme.breakpoints.phone
     return (
       <Container>
-        <Particles
-          className='particles'
-          params={{
-            retina_detect: true,
-            particles: {
-              color: {
-                value: '#fff',
-              },
-              number: {
-                value: 160,
-              },
-              size: {
-                value: 2,
-              },
-              line_linked: {
-                enable: true,
-                distance: 50,
-                color: '#dfbb4f',
-                opacity: 0.4,
-                width: 1,
-              },
-              move: {
-                enable: true,
-                speed: 6,
-                direction: 'none',
-                random: false,
-                straight: false,
-                out_mode: 'out',
-                bounce: true,
-                attract: {
+        {showParticles && (
+          <Particles
+            className='particles'
+            params={{
+              retina_detect: true,
+              particles: {
+                color: {
+                  value: '#fff',
+                },
+                number: {
+                  value: 160,
+                },
+                size: {
+                  value: 2,
+                },
+                line_linked: {
                   enable: true,
-                  rotateX: 600,
-                  rotateY: 1200,
+                  distance: 50,
+                  color: '#dfbb4f',
+                  opacity: 0.4,
+                  width: 1,
+                },
+                move: {
+                  enable: true,
+                  speed: 6,
+                  direction: 'none',
+                  random: false,
+                  straight: false,
+                  out_mode: 'out',
+                  bounce: true,
+                  attract: {
+                    enable: true,
+                    rotateX: 600,
+                    rotateY: 1200,
+                  },
                 },
               },
-            },
 
-            interactivity: {
-              events: {
-                onhover: {
-                  enable: true,
-                  mode: ['bubble', 'repulse'],
+              interactivity: {
+                events: {
+                  onhover: {
+                    enable: true,
+                    mode: ['bubble', 'repulse'],
+                  },
+                },
+                modes: {
+                  bubble: { size: 5 },
+                  repulse: { distance: 50 },
                 },
               },
-              modes: {
-                bubble: { size: 5 },
-                repulse: { distance: 50 },
-              },
-            },
-          }}
-        />
+            }}
+          />
+        )}
       </Container>
     )
   }
 }
 
-export default SplashBackground
+export default withTheme(SplashBackground)
